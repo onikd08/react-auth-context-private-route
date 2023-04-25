2d30e1db625ea59353560f60e5fc2513ec6d25e8
Anik Das <onikd08@gmail.com>
2023-04-25 03:11:02 +0300

    Add login and register form and handler

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -9,13 +16,14 @@ const Login = () => {
             <h1 className="text-5xl font-bold">Please Login now!</h1>
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form onSubmit={handleSubmit} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
                   required
+                  name="email"
                   type="email"
                   placeholder="email"
                   className="input input-bordered"
@@ -27,6 +35,7 @@ const Login = () => {
                 </label>
                 <input
                   required
+                  name="password"
                   type="password"
                   placeholder="password"
                   className="input input-bordered"
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,72 @@
 import React from "react";
 
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, email, password);
+  };
+
   return (
     <div>
-      <h2>Register</h2>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Please Register!</h1>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">name</span>
+                </label>
+                <input
+                  required
+                  name="name"
+                  type="text"
+                  placeholder="Your Name"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  required
+                  name="email"
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  required
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Register</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
