85d9e98c41d172bbf15a004736735f6b2f499be3
Anik Das <onikd08@gmail.com>
2023-04-25 04:07:48 +0300

    clear form after handlesubmit

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -15,6 +15,7 @@ const Register = () => {
       .then((result) => {
         const user = result.user;
         console.log(user);
+        form.reset();
       })
       .catch((error) => {
         console.log(error.message);
